52abb55afb66413a66d6db4567e3532d61448a39
helloberries <helloberriesqz@gmail.com>
2016-09-17 17:43:18 +0800

    display the pic which is been clicked

--- js/script.js
+++ js/script.js
@@ -1,35 +1,40 @@
 $(() => {
 
 	let model = {
+		addHits(currCatName) {
+			for (let elem of this.data()) {
+				if (elem.name === currCatName) {
+					elem.hits++;
+					console.log(elem);
+				}
+			}
+		},
 		data() {
 			return [
 				{
 					name: 'cat',
-					src: 'img/cat1.jpg'
+					src: 'img/cat1.jpg',
+					hits: 0
 				},
 				{
 					name: 'purr',
-					src: 'img/cat2.jpg'
+					src: 'img/cat2.jpg',
+					hits: 0
+
 				},
 				{
 					name: 'meow',
-					src: 'img/cat3.jpg'
+					src: 'img/cat3.jpg',
+					hits: 0
+
 				},
 				{
 					name: 'claws',
-					src: 'img/cat4.jpg'
+					src: 'img/cat4.jpg',
+					hits: 0
+
 				}
 			];	
-		},
-		catBeenChosen() { 
-			$('.cat-btn').on('click', (event) => {
-				let catName = event.target.text();
-				for (let elem of model.data()) {
-					if (elem.name === catName) {
-						return elem.src;
-					}
-				}
-			});
 		}
 	};
 
@@ -37,11 +42,15 @@ $(() => {
 		getAllCats() {
 			return model.data();
 		},
-		getCurrCat() {
-			return model.catBeenChosen();
+		getCatDetail(key, refer, get) {
+			for (let elem of octopus.getAllCats()) {
+				if (elem[key] === refer) {
+					return elem[get];
+				}
+			}
 		},
-		showCurrCatHits() {
-
+		addCatHits(currCatName) {
+			model.addHits(currCatName);
 		},
 		init() {
 			viewOfList.init();
@@ -62,8 +71,39 @@ $(() => {
 
 	let viewOfDisplay = {
 		init() {
-			let currCatSrc = octopus.getCurrCat();
+			let currCatSrc = octopus.getAllCats()[0].src;
 			$('#showarea .catpic').attr('src', currCatSrc);
+			viewOfDisplay.render();
+			viewOfDisplay.showHits();
+		},
+		render() {
+
+			$('.cat-btn').on('click', (e) => {
+				let catName = $(e.target).text();
+				let currCatSrc = octopus.getCatDetail('name', catName, 'src');
+				$('#showarea .catpic').attr('src', currCatSrc);
+			});
+
+		},
+		showHits() {
+			this.counterShow = $('#counterShow');
+
+			this.counterShow.text(0);
+
+			$('.cat-btn').on('click', (e) => {
+				let catName = $(e.target).text();
+
+				octopus.addCatHits(catName);
+
+				let currCatHits = octopus.getCatDetail('name', catName, 'hits');
+
+				this.counterShow.text(currCatHits);
+
+				// test
+				for (let elem of model.data()) {
+					console.log(elem.hits);
+				}
+			});
 		}
 	};
 
