80b747ae490a76cad4f5bd33cee7dbffdea6cd5c
helloberries <helloberriesqz@gmail.com>
2016-09-18 16:25:23 +0800

    add new pic from form

--- js/script.js
+++ js/script.js
@@ -2,8 +2,11 @@ $(() => {
 
 	let model = {
 		currentCat: null,
-		data() {
-			return [
+		adminStat: false,
+		add(obj) {
+			this.data.push(obj);
+		},
+		data: [
 				{
 					name: 'cat',
 					src: 'img/cat1.jpg',
@@ -27,21 +30,12 @@ $(() => {
 					hits: 0
 
 				}
-			];	
-		}
+			]
 	};
 
 	let octopus = {
-		init() {
-			// set the current cat to the first cat
-			model.currentCat = model.data()[0];
-
-			// initialize the views
-			viewOfList.init();
-			viewOfDisplay.init();
-		},
 		getAllCats() {
-			return model.data();
+			return model.data;
 		},
 		getCurrCat() {
 			return model.currentCat;
@@ -52,7 +46,24 @@ $(() => {
 		addHits() {
 			model.currentCat.hits++;
 			viewOfDisplay.render();
+		},
+		addCat(newCat) {
+			model.add(newCat);
+			viewOfList.render();
+		},
+		changeAdminStat() {
+			model.adminStat = !model.adminStat;
+		},
+		init() {
+			// set the current cat to the first cat
+			model.currentCat = model.data[0];
+
+			// initialize the views
+			viewOfList.init();
+			viewOfDisplay.init();
+			viewOfAdmin.init();
 		}
+
 	};
 
 	let viewOfList = {
@@ -65,6 +76,7 @@ $(() => {
 			let cats = octopus.getAllCats();
 
 			// empty the cat list
+			this.catslist.html('');
 
 			// loop over the cats
 			for (let cat of cats) {
@@ -113,6 +125,47 @@ $(() => {
 
 		}
 	};
+
+	let viewOfAdmin = {
+		init() {
+			// pointers
+			this.adminBtn = $('#adminBtn');
+			this.adminForm = $('#adminForm');
+			this.saveBtn = $('#save');
+
+			// hide the form at first
+			this.adminForm.hide();
+
+			// click the admin button, show the form
+			this.adminBtn.on('click', () => {
+				this.adminForm.toggle();
+				octopus.changeAdminStat();
+			});
+
+			// click the save button, add new cat to the model
+			this.adminForm.submit((e) => {
+
+				e.preventDefault();
+
+				this.nameVal = $('#name').val();
+				this.srcVal = $('#src').val();
+				this.hitsVal = $('#hits').val();
+
+				let newCat = {
+					name: this.nameVal,
+					src: this.srcVal,
+					hits: this.hitsVal
+				};
+
+				octopus.addCat(newCat);
+
+
+				this.nameVal = $('#name').val('');
+				this.srcVal = $('#src').val('');
+				this.hitsVal = $('#hits').val('');
+			});
+		}
+	}
 
 	octopus.init();
 
